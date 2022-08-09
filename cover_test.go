e8a13cb5f991c3650de38b0593e09cad1a12c3b6
burntcarrot <aadhav.n1@gmail.com>
2022-08-09 18:35:29 +0530

    tests: cleanup tests

---
+++ cover_test.go
@@ -0,0 +1,34 @@
+package samosa
+
+import (
+	"testing"
+)
+
+func TestGetCoverageData(t *testing.T) {
+	type test struct {
+		description string
+		filename    string
+		wantErr     bool
+	}
+
+	tests := []test{
+		{
+			description: "valid coverage report",
+			filename:    "./testdata/test_getcoveragedata/test_coverage.txt",
+		},
+		{
+			description: "invalid path",
+			filename:    "",
+			wantErr:     true,
+		},
+	}
+
+	t.Run("", func(t *testing.T) {
+		for _, tc := range tests {
+			_, _, _, err := GetCoverageData(tc.filename)
+			if err != nil && !tc.wantErr {
+				t.Fatalf("test failed (%s): %v\n", tc.description, err.Error())
+			}
+		}
+	})
+}
--- file_walk_test.go
+++ file_walk_test.go
@@ -7,15 +7,65 @@ import (
 	"golang.org/x/tools/cover"
 )
 
-func TestWalk(t *testing.T) {
-	got := map[string]interface{}{}
-	data, err := getRoot()
-	if err != nil {
-		t.Fatalf("no err expected:%v\n", err)
+func TestWalkModDir(t *testing.T) {
+	type test struct {
+		description string
+		filename    string
+		want        []string
+		wantErr     bool
 	}
-	decodeJSON(data, got)
-	want := getModDir(got)
-	_, err = walkModDir(want)
+
+	tests := []test{
+		{
+			description: "valid directory",
+			filename:    "./testdata/test_walkmoddir/go.mod.txt",
+			want:        []string{"sample_moddir.go", "go.mod.txt", "sample_dir"},
+		},
+		{
+			description: "invalid path",
+			filename:    "",
+			wantErr:     true,
+		},
+	}
+
+	for _, tc := range tests {
+		got, err := walkModDir(tc.filename)
+		if err != nil && !tc.wantErr {
+			t.Fatalf("test failed (%s): %v\n", tc.description, err.Error())
+		}
+
+		diff := cmp.Diff(got, tc.want)
+		if len(diff) > 0 {
+			t.Fatalf("got != want; got = %v, want = %v\n", got, tc.want)
+		}
+	}
+}
+
+func TestWalkDir(t *testing.T) {
+	type test struct {
+		description string
+		filename    string
+		wantErr     bool
+	}
+
+	tests := []test{
+		{
+			description: "valid directory",
+			filename:    "./testdata/test_walkmoddir/go.mod.txt",
+		},
+		{
+			description: "invalid path",
+			filename:    "",
+			wantErr:     true,
+		},
+	}
+
+	for _, tc := range tests {
+		_, err := walkDir()
+		if err != nil && !tc.wantErr {
+			t.Fatalf("test failed (%s): %v\n", tc.description, err.Error())
+		}
+	}
 }
 
 func TestProfile(t *testing.T) {
@@ -23,6 +73,7 @@ func TestProfile(t *testing.T) {
 		description string
 		filename    string
 		want        []*cover.Profile
+		wantErr     bool
 	}
 
 	tests := []test{
@@ -64,11 +115,16 @@ func TestProfile(t *testing.T) {
 			filename:    "./testdata/test_profile/test_coverage_missing.txt",
 			want:        []*cover.Profile{},
 		},
+		{
+			description: "invalid path",
+			filename:    "",
+			wantErr:     true,
+		},
 	}
 
 	for _, tc := range tests {
 		got, err := getProfiles(tc.filename)
-		if err != nil {
+		if err != nil && !tc.wantErr {
 			t.Fatalf("test failed (%s): %v\n", tc.description, err.Error())
 		}
 
@@ -79,20 +135,51 @@ func TestProfile(t *testing.T) {
 	}
 }
 
-func TestFilenames(t *testing.T) {
-	profiles, err := getProfiles("./testdata/test_coverage.txt")
-	if err != nil {
-		t.Log(err)
+func TestGetFunctionInfo(t *testing.T) {
+	type test struct {
+		description string
+		profiles    []*cover.Profile
+		wantErr     bool
 	}
-	_, _, _, err = getFunctionInfo(profiles)
-	if err != nil {
-		t.Fatal("no error expected")
-	}
-}
 
-func TestWalkFile(t *testing.T) {
-	_, err := walkDir()
-	if err != nil {
-		t.Fatal("no error expected")
+	tests := []test{
+		{
+			description: "valid profile",
+			profiles: []*cover.Profile{
+				{
+					FileName: "github.com/burntcarrot/samosa/cover.go",
+					Mode:     "atomic",
+					Blocks: []cover.ProfileBlock{
+						{
+							StartLine: 3,
+							StartCol:  78,
+							EndLine:   5,
+							EndCol:    16,
+							NumStmt:   2,
+							Count:     0,
+						},
+						{
+							StartLine: 9,
+							StartCol:  2,
+							EndLine:   10,
+							EndCol:    16,
+							NumStmt:   2,
+							Count:     0,
+						},
+					},
+				},
+			},
+		},
+		{
+			description: "empty profile",
+			wantErr:     true,
+		},
+	}
+
+	for _, tc := range tests {
+		_, _, _, err := getFunctionInfo(tc.profiles)
+		if err != nil && !tc.wantErr {
+			t.Fatalf("test failed (%s): %v\n", tc.description, err.Error())
+		}
 	}
 }
